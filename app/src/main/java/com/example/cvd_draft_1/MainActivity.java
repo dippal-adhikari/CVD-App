a526761c458b2630a294404b34923a98c1572613
dippal-adhikari <adhikari.dippal@gmail.com>
2024-09-04 13:02:29 +1000

    profileActivity

--- app/src/main/java/com/example/cvd_draft_1/MainActivity.java
+++ app/src/main/java/com/example/cvd_draft_1/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.cvd_draft_1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,4 +24,17 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+
+    Button btnViewEditProfile = findViewById(R.id.btnViewEditProfile);
+    btnViewEditProfile.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+            startActivity(intent);
+        }
+    });
+
+
+
 }
+
