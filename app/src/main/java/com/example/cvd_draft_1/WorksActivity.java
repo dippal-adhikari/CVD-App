a2346659aa22e28d1ccb5133a2b7bb06df421d9b
Sneha <99473269+snehbom@users.noreply.github.com>
2024-09-11 22:19:40 +1000

    changeLanguage

--- app/src/main/java/com/example/cvd_draft_1/WorksActivity.java
+++ app/src/main/java/com/example/cvd_draft_1/WorksActivity.java
@@ -1,15 +1,22 @@
 package com.example.cvd_draft_1;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.content.res.Configuration;
+import java.util.Locale;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -38,6 +45,8 @@ public class WorksActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_works);
 
+        loadLocale();
+
         //Initialize FirebaseAuth instance
         mAuth = FirebaseAuth.getInstance();
 
@@ -54,9 +63,70 @@ public class WorksActivity extends AppCompatActivity {
                 Intent intent = new Intent(WorksActivity.this, LauncherActivity.class);
                 startActivity(intent);
             }
+
         });
 
 
+        Button btnChangeLanguage = findViewById(R.id.btnChangeLanguage);
+        btnChangeLanguage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeLanguage();
+            }
+        });
+
 
     }
+
+    private void changeLanguage() {
+        final String languages[] = {"English", "français","deutsch", "Italiano"};
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(this);
+        mBuilder.setTitle("Choose Language");
+        mBuilder.setSingleChoiceItems(languages, -1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (i == 0) {
+                    setLocale("");
+                    recreate();
+                }
+                else if (i == 1) {
+                    setLocale("fr");
+                    recreate();
+                }
+                else if (i == 2) {
+                    setLocale("nl");
+                    recreate();
+                }
+                else if (i == 3) {
+                    setLocale("it");
+                    recreate();
+                }
+            }
+        });
+        mBuilder.create();
+        mBuilder.show();
+    }
+
+    private void setLocale(String language) {
+        Locale locale = new Locale(language);
+        Locale.setDefault(locale);
+
+        Configuration configuration = new Configuration();
+        configuration.locale = locale;
+        getBaseContext().getResources().updateConfiguration(configuration, getBaseContext().getResources().getDisplayMetrics());
+
+        SharedPreferences.Editor editor = getSharedPreferences("Settings", MODE_PRIVATE).edit();
+        editor.putString("CVD-Draft-1", language);
+        editor.apply();
+
+    }
+    private void loadLocale(){
+        SharedPreferences preferences= getSharedPreferences("Settings", MODE_PRIVATE);
+        String language = preferences.getString("CVD-Draft-1", "");
+        setLocale(language);
+
+
+    }
+
+
 }
